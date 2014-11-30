cd81de42b02db68237a2358ac8904966281cd510
kskovpen <Kirill.Skovpen@cern.ch>
2014-11-30 13:56:52 +0100

    update

--- TreeReader/runTreeReader.C
+++ TreeReader/runTreeReader.C
@@ -4,19 +4,19 @@
   
   std::vector<TString > systlist;
   systlist.push_back("");		
-  /*systlist.push_back("lept__plus");
+/*  systlist.push_back("lept__plus");
   systlist.push_back("lept__minus" );    
-  //systlist.push_back("trig__plus");
-  //systlist.push_back("trig__minus" );  
-  //systlist.push_back("PDF__plus");     
-  //systlist.push_back("PDF__minus");    
+  systlist.push_back("trig__plus");
+  systlist.push_back("trig__minus" );*/
+/*  systlist.push_back("PDF__plus");     
+  systlist.push_back("PDF__minus");    
   systlist.push_back("jes__plus");
   systlist.push_back("jes__minus");      
   systlist.push_back("jer__plus");
   systlist.push_back("jer__minus");   
   systlist.push_back("metuncls__plus");
-  systlist.push_back("metuncls__minus");
-  */
+  systlist.push_back("metuncls__minus");*/
+  
   
   
   std::vector<TString > datalist;
